041f2a48f68eb2d459fceb3de8e2eb78d829821d
Abhay-dev-hub <leenawaghmode@gmail.com>
2025-02-20 14:05:40 +0530

    Add files via upload

---
+++ codeforces/Team.py
@@ -0,0 +1,19 @@
+t = int(input())
+p = 0
+for i in range(0,t):
+    x,y,z = map(int,input().split())
+    if x+y+z >= 2:
+        p += 1
+    else:
+
+        pass
+print(p)
+    
+        
+
+
+    
+
+
+
+
