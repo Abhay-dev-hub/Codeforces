fe49f7e4feb45d6706c0b3a6869ee5d2f1d2c9ba
Abhay-dev-hub <leenawaghmode@gmail.com>
2025-03-27 16:12:32 +0530

    corrected broze.py

--- python/broze.py
+++ python/broze.py
@@ -1,12 +1,16 @@
 code = str(input())
 res = ""
-for i in range(0,len(code)-1):
-    if code[i] and code[i+1] == "-.":
-        res += "1"
-
-    if code[i] and code[i+1] == "--":
-        res += "2"
-    
-    if code[i] ==".":
+i = 0
+while i < len(code):
+    if code[i] == ".":
         res += "0"
+        i += 1
+    elif i + 1 < len(code) and code[i] == "-" and code[i+1] == ".":
+        res += "1"
+        i += 2
+        # he condin true zali amhnun i += 2 kela 
+    elif i + 1 < len(code) and code[i] == "-" and code[i+1] == "-":
+        res += "2"
+        i += 2
+
 print(res)
